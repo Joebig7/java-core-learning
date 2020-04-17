d9ad9bdb953355da5e9706ddfd4c907c7107034c
JoeBig7 <1479762535@qq.com>
2020-04-17 18:00:35 +0800

    fix:update

--- src/main/java/concurrency/collection/concurrenthashmap/Bootstrap.java
+++ src/main/java/concurrency/collection/concurrenthashmap/Bootstrap.java
@@ -1,6 +1,5 @@
 package concurrency.collection.concurrenthashmap;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -10,57 +9,35 @@ import java.util.concurrent.ConcurrentHashMap;
  * @description ConcurrentHashMap使用案例
  */
 public class Bootstrap {
-
-
-    private static ConcurrentHashMap<Integer, Student> map = new ConcurrentHashMap<>(12);
-//    private static HashMap<Integer, Student> map = new HashMap<>();
-
+    private static ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+//    private static HashMap<String, String> map = new HashMap<>();
 
     public static void main(String[] args) {
 
-        Student stu1 = new Student("stu1", 25);
-        Student stu2 = new Student("stu2", 22);
-        Student stu3 = new Student("stu3", 26);
-
-        map.put(1, stu1);
-        map.put(2, stu2);
-        map.put(3, stu3);
-
-
         Thread thread1 = new Thread(() -> {
-            for (Map.Entry<Integer, Student> entry : map.entrySet()) {
-                System.out.println("id:" + entry.getKey() + "----" + "age:" + entry.getValue().getName());
+            for (int i = 0; i < 400; i++) {
+                map.put(String.valueOf(i), String.valueOf(i));
+            }
+
+
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                map.put(String.valueOf(i), String.valueOf(i));
             }
         });
 
-
-        Thread thread2 = new Thread(() -> {
-            int age = 21;
-            for (int i = 0; i < 10; i++) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                map.put(i + 4, new Student("stu" + (i + 4), age));
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 400; i++) {
+                map.remove(String.valueOf(i));
             }
         });
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("finish");
-
-        for (Map.Entry<Integer, Student> entry : map.entrySet()) {
-            System.out.println("id:" + entry.getKey() + "----" + "name:" + entry.getValue().getName());
-        }
 
     }
 
