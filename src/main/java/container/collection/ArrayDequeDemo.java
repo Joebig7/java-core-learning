a89dc8faf91e88493a074455633fb99a80a84ec5
JoeBig7 <1479762535@qq.com>
2021-05-10 17:58:41 +0800

    :sparles: update ArrayDeque

--- src/main/java/container/collection/ArrayDequeDemo.java
+++ src/main/java/container/collection/ArrayDequeDemo.java
@@ -1,7 +1,5 @@
 package container.collection;
 
-import com.sun.jmx.remote.internal.ArrayQueue;
-
 import java.util.ArrayDeque;
 
 /**
@@ -10,11 +8,19 @@ import java.util.ArrayDeque;
  */
 public class ArrayDequeDemo {
 
-    private  static
-         ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+    private static
+    ArrayDeque<String> arrayDeque = new ArrayDeque<>();
 
 
     public static void main(String[] args) {
-        arrayDeque.addFirst("a");
+        arrayDeque.add("a");
+        arrayDeque.addFirst("b");
+        arrayDeque.addLast("c");
+        arrayDeque.push("d");
+//        arrayDeque.push();
+
+        System.out.println(arrayDeque.pop());
+        System.out.println(arrayDeque.getFirst());
+        System.out.println(arrayDeque.poll());
     }
 }
